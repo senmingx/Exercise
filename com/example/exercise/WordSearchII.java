d573ab931758e65151dfc62fecb08be9bb32c2df
senmingx <senmingx0416@gmail.com>
2021-10-09 12:34:24 -0700

    WordSearchII using Trie

--- com/example/exercise/WordSearchII.java
+++ com/example/exercise/WordSearchII.java
@@ -1,84 +1,60 @@
 package com.example.exercise;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 public class WordSearchII {
 
     private static final int[][] DIRS = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
 
-    // Number of words: m
-    // Word length: n
-    //
-    // Time: O(m * 4^n)
-    // space: O(row * col)
+    // Assumptions:
+    // 1. board[i][j] is a lowercase English letter.
+    // 2. words[i] consists of lowercase English letters.
+    // 3. 1 <= m, n <= 12
+    // 4. 1 <= words[i].length <= 10, 1 <= words.length <= 3 * 104
     public List<String> findWords(char[][] board, String[] words) {
-        List<String> res = new ArrayList<>();
-        // Corner cases
-        if (board == null || board.length == 0 || board[0] == null || board[0].length == 0 || words == null) {
-            throw new IllegalArgumentException();
-        }
-        if (words.length == 0) {
-            return res;
-        }
+        int m = board.length, n = board[0].length;
+        List<String> wordsFound = new ArrayList<>();
 
+        // 1. Add all words to trie
+        // 2. Start with each cell, find in trie startWith prefix to find words
+        //    Each time a word is found, delete it from trie to avoid duplicate
+
+        Trie trie = new Trie();
         for (String word : words) {
-            if (findWord(board, word)) {
-                res.add(word);
+            trie.insert(word);
+        }
+
+        for (int row = 0; row < m; row++) {
+            for (int col = 0; col < n; col++) {
+                StringBuilder sb = new StringBuilder();
+                findWords(board, row, col, sb, trie, wordsFound);
             }
         }
 
-        return res;
+        return wordsFound;
     }
 
-    private boolean findWord(char[][] board, String word) {
-        int rows = board.length, cols = board[0].length;
-        for (int row = 0; row < rows; row++) {
-            for (int col = 0; col < cols; col++) {
-                boolean[][] visited = new boolean[rows][cols];
-                if (findWord(board, row, col, word, 0, visited)) {
-                    return true;
-                }
-            }
+    private void findWords(char[][] board, int row, int col, StringBuilder sb, Trie trie, List<String> wordsFound) {
+        if (trie.search(sb.toString())) {
+            wordsFound.add(sb.toString());
+            trie.delete(sb.toString());
         }
-        return false;
-    }
-
-    private boolean findWord(char[][] board, int row, int col, String word, int index, boolean[][] visited) {
-        int rows = board.length, cols = board[0].length;
-        // Base case 1: Found word (Put this first as we could be out of bound but still found word)
-        if (index == word.length()) {
-            return true;
-        }
-        // Base case 0: Our of bound or Already visited
-        if (row < 0 || row >= rows || col < 0 || col >= cols || visited[row][col]) {
-            return false;
-        }
-        // Base case 2: Letter not match
-        if (board[row][col] != word.charAt(index)) {
-            return false;
+        int m = board.length, n = board[0].length;
+        if (row < 0 || row >= m || col < 0 || col >= n || board[row][col] == '*' || !trie.startsWith(sb.toString())) {
+            return;
         }
 
-        visited[row][col] = true;
+        char cur = board[row][col];
+        sb.append(cur);
+        board[row][col] = '*';
 
-        // Try each direction
         for (int[] dir : DIRS) {
             int nextRow = row + dir[0];
             int nextCol = col + dir[1];
-            if (findWord(board, nextRow, nextCol, word, index + 1, visited)) {
-                return true;
-            }
+            findWords(board, nextRow, nextCol, sb, trie, wordsFound);
         }
 
-        visited[row][col] = false;
-
-        return false;
-    }
-
-    public static void main(String[] args) {
-        WordSearchII sol = new WordSearchII();
-
+        sb.deleteCharAt(sb.length() - 1);
+        board[row][col] = cur;
     }
 }
